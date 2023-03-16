fc01271e4cf20c153e61db9a76acaf5050af6933
ax-hack <ax-hack@pm.me>
2023-03-16 17:48:45 +0300

    just test

---
+++ mcquiz/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="screen--quiz" id="quiz">
+    <div class="section">
+      <router-view />
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scope>
+ .screen--quiz .section-title {
+   font-family: 'Noto Sans';
+   font-style: normal;
+   font-weight: 900;
+   font-size: 25px;
+   line-height: 34px;
+
+   text-align: right;
+   
+   color: #F8E9CB;
+ }
+ .screen--quiz .pitch {
+   font-family: 'Noto Sans';
+   font-style: normal;
+   font-weight: 400;
+   font-size: 25px;
+   line-height: 34px; 
+
+   color: #666460;
+ }
+</style>
---
+++ mcquiz/src/modules/example/pages/Example.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+ import { onMounted, ref } from "vue";
+
+ const welcome = ref("Boilerplate Vue 3 + Vite + TypeScript + Pinia");
+</script>
+
+<template>
+  <h2 class="section-title">За 1 минуту узнайте какое решением Вам подойдёт</h2>
+  <p class="pitch">Без ввода контактных данных</p>
+  
+ 
+</template>
